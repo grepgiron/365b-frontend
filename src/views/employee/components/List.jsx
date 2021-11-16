910f9f1003cb737812e84bc9d3bc992a59198e04
DESKTOP-QKMRI21\Lucario <djosechavarria7@gmail.com>
2021-11-16 01:10:12 -0600

    Hice unos cambios en la tabla de empleados

--- src/views/employee/components/List.jsx
+++ src/views/employee/components/List.jsx
@@ -6,20 +6,31 @@ import {
   Pagination
 } from 'rsuite';
 
-import value from './sample';
+
+//import value from './sample';
 
 const { HeaderCell, Cell, Column, ColumnGroup } = Table;
 
 function List() {
-  const [clientsArray, setClientsArray] = React.useState(null);
+  const [clientsArray, setClientsArray] = React.useState([]);
+  const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [limit, setLimit] = React.useState(10);
   const [page, setPage] = React.useState(1);
 
   useEffect(() => {
       // GET request using axios with async/await
-      axios.get('https://beauty365api.herokuapp.com/api/v1/clientes')
-        .then((response) => setClientsArray)
+      fetch('https://beauty365api.herokuapp.com/api/v1/empleados')
+        .then(response => response.json())
+        .then((result ) => {
+          setClientsArray(result)
+          console.log(result)
+        }, (error) => { setError(error)
+          console.log(error)
+
+        }
+        
+        )
       console.log(clientsArray)
   }, []);
     
@@ -29,59 +40,36 @@ function List() {
       setLimit(dataKey);
     };
 
-    const data = value.filter((v, i) => {
+   /* const data = clientsArray.filter((v, i) => {
       const start = limit * (page - 1);
       const end = start + limit;
       return i >= start && i < end;
-    });
+    });*/
   
     return (
       <>
-        {console.log(axios.get('https://beauty365api.herokuapp.com/api/v1/clientes'))}
-        <Table height={420} data={data} loading={loading}>
+       
+        <Table height={420} data={clientsArray} loading={loading}>
           <Column width={50} align="center" fixed>
             <HeaderCell>Id</HeaderCell>
-            <Cell dataKey="id" />
+            <Cell dataKey="_id" />
           </Column>
 
-          <Column width={100} fixed>
+          <Column  flexGrow>
             <HeaderCell>Primer Nombre</HeaderCell>
-            <Cell dataKey="firstName" />
-          </Column>
-
-          <Column width={100}>
-            <HeaderCell>Segundo Nombre</HeaderCell>
-            <Cell dataKey="lastName" />
+            <Cell dataKey="nombres" />
           </Column>
 
           <Column width={200}>
-            <HeaderCell>City</HeaderCell>
-            <Cell dataKey="city" />
+            <HeaderCell>Telefono</HeaderCell>
+            <Cell dataKey="telefono" />
           </Column>
-          <Column width={200} flexGrow={1}>
-            <HeaderCell>Compa;ia</HeaderCell>
-            <Cell dataKey="companyName" />
+          <Column >
+            <HeaderCell>DNI</HeaderCell>
+            <Cell dataKey="dni" />
           </Column>
         </Table>
-        <div style={{ padding: 20 }}>
-          <Pagination
-            prev
-            next
-            first
-            last
-            ellipsis
-            boundaryLinks
-            maxButtons={5}
-            size="xs"
-            layout={['total', '-', 'limit', '|', 'pager', 'skip']}
-            total={value.length}
-            limitOptions={[10, 20]}
-            limit={limit}
-            activePage={page}
-            onChangePage={setPage}
-            onChangeLimit={handleChangeLimit}
-          />
-        </div>
+        
       </>
     );
 }
