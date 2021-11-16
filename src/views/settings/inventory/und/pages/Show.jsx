de79625f8ccaa090f550cfc12a589d9b952cc310
grepgiron <gregoriopaguada@outlook.com>
2021-11-15 20:19:06 -0600

    arreglos

--- src/views/settings/inventory/und/pages/Show.jsx
+++ src/views/settings/inventory/und/pages/Show.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
@@ -11,7 +11,30 @@ import Profile from '../components/Profile'
 
 const Show = (props) => {
   let { id } = useParams();
+  const [ establecimiento, setEstablecimiento] = React.useState(null);
+
   console.log(id)
+
+  useEffect(() => {
+    fetch("https://beauty365api.herokuapp.com/api/v1/establecimientos")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          //setIsLoaded(true);
+          setEstablecimiento(result);
+          console.log(result);
+        },
+        // Nota: es importante manejar errores aquí y no en 
+        // un bloque catch() para que no interceptemos errores
+        // de errores reales en los componentes.
+        (error) => {
+          //setIsLoaded(true);
+          //setError(error);
+          console.log(error);
+        }
+      )
+  })
+
   return (
     <>
     <div>
